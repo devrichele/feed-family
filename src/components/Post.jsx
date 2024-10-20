5f70ec7c4aae500550e25d607c5e01e9fa6d446c
Richele <brendaricheledev@gmail.com>
2024-10-20 13:07:37 -0300

    Criando componente - aprendendo sobre a utilização da tag time

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,26 @@
+import styles from './Post.module.css';
+
 export function Post (){
     return(
-        <h1> helo </h1>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avart}src="https://media.licdn.com/dms/image/v2/D4D03AQH_z--8pl-nnA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1722022791055?e=1734566400&v=beta&t=8MxIeqQftTASwCElqIAG27ZqwLAEeJGQG-20JlLwCMU"/>
+                 <div className={styles.authorInfo}>
+                        <strong>Richele B</strong>
+                        <span>Web Devloper</span>
+
+                    </div>
+                </div>
+                <time title="20 de Outubro ás 13h" dateTime="2024-20-10 13:00:10">Publicado há 1h</time>
+
+             </header>
+             <div className={styles.content}>
+                <p>
+                    Olá Pessoal, estou amando aprender mais sobre criação de componente.
+                    Cada dia que passa, eu me apaixono mais um pouco pela programação.
+                </p>
+             </div>
+        </article>
     )
 }
